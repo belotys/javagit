a5ef658643b82268c755dc2cf23fde9ade0a122c
belothys <michel.benoit.dev@gmail.com>
2015-11-07 14:23:09 +0100

    test Day of week
    test de quelques méthodes

--- src/main/java/be/noit/michel/DateTest.java
+++ src/main/java/be/noit/michel/DateTest.java
@@ -1,5 +1,23 @@
 package be.noit.michel;
 
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class DateTest {
 
+	public static void main(String[] args) {
+		
+		DayOfWeek dw = DayOfWeek.MONDAY;
+		
+		System.out.println(dw.getDisplayName(TextStyle.FULL, Locale.FRANCE));
+		System.out.println(dw.getDisplayName(TextStyle.NARROW, Locale.FRANCE));
+		System.out.println(dw.getDisplayName(TextStyle.SHORT, Locale.FRANCE));
+		
+		System.out.println(dw.minus(3).getDisplayName(TextStyle.FULL, Locale.FRANCE));
+		
+		
+		
+		
+	}
 }
